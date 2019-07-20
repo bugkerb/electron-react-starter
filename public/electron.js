2ccd445ba18cb05052d5aabd1669d733c24d1bfb
Jiratthapol Aroyvithee <jiratthapol@gmail.com>
2019-07-20 18:14:05 +0700

    add serial port feature

--- public/electron.js
+++ public/electron.js
@@ -8,8 +8,19 @@ const isDev = require('electron-is-dev');
 let mainWindow;
 
 function createWindow() {
-  mainWindow = new BrowserWindow({width: 900, height: 680, autoHideMenuBar:true});
+  mainWindow = new BrowserWindow({
+    width: 900, 
+    height: 680, 
+    autoHideMenuBar:true,
+    webPreferences: {
+      nodeIntegration: true,
+     // preload: path.join(__dirname, 'renderer.js'),
+      webSecurity: !isDev
+    }
+  });
+  
   mainWindow.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`);
+  
   if (isDev) {
     // Open the DevTools.
     BrowserWindow.addDevToolsExtension(path.join(__dirname, 'chrome-extensions', 'react-dev-tools'));
